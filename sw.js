cb81349dbe43322e73ad92eb68cbef99c9b6b5e1
Jonathan Coral <115955802+jonathancoral@users.noreply.github.com>
2026-02-04 15:58:54 -0500

    Update service worker caching strategy and assets

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const cacheName = 'segsocial-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'segsocial-m3-v3';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
+    'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,300;400;500;600&display=swap',
+    'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    )));
 });
